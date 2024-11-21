d9d70b1a9b32ca501900d1e021cbcd38aeda1656
Ildus Akhmadiev <anan308an@gmail.com>
2024-11-21 17:20:41 +0300

    init

---
+++ src/main/java/com/github/ildus_akhmadiev/audiocomparison/AudioComparisonApplication.java
@@ -0,0 +1,13 @@
+package com.github.ildus_akhmadiev.audiocomparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioComparisonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudioComparisonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ildus_akhmadiev/audiocomparison/AudioComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ildus_akhmadiev.audiocomparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioComparisonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
